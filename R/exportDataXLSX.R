44221a0faab1046cb80de08a359b06f5622a0e4d
tonedivad <notinaber@gmail.com>
2015-10-12 11:51:50 +0200

    modified:   R/exportDataXLSX.R

--- R/exportDataXLSX.R
+++ R/exportDataXLSX.R
@@ -44,8 +44,8 @@ exportDataXLSX<-function(obj,outfile,ldata=names(obj$Data),sortSid=c("sType","In
     if(!transpose) m=round(obj$Data[[i]][lsoSa,lsoAna,drop=FALSE],nround)
     m=apply(m,2,as.character)
 #    print(str(m))
-    if(transpose) df=cbind("Analyte"=obj$Analyte,m,stringsAsFactors=FALSE)
-    if(!transpose) df=cbind("Sid"=obj$Sid,m,stringsAsFactors=FALSE)
+    if(transpose) df=cbind("Analyte"=obj$Analyte[lsoAna],m,stringsAsFactors=FALSE)
+    if(!transpose) df=cbind("Sid"=obj$Sid[lsoSa],m,stringsAsFactors=FALSE)
     #   print(names(df))
     df=rbind(colnames(df),df)
     df[is.na(df)]=characterNA[2]

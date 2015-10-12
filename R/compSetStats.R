ca6c5b0e72c9ba8a6a3e0cf2a36ddd7fd4c2e76e
tonedivad <notinaber@gmail.com>
2015-10-12 14:18:01 +0200

    modified:   R/compSetStats.R 	modified:   man/compSetStats.Rd

--- R/compSetStats.R
+++ R/compSetStats.R
@@ -5,19 +5,29 @@ compSetStats<-function(obj,lStats=list(),whatSamp="Sa",whatQC="QC",add=FALSE){
   cvrf<-function(x,n=0) ifelse(sum(!is.na(x))>=n,100*mad(x,na.rm=T)/median(x,na.rm=T),NA)
   
 
-  SaData=obj$Data
-  if(length(whatSamp)>0 & whatSamp%in%obj$Meta$Sid) SaData=lapply(obj$Data,function(x) x[which(obj$Meta$Sid%in%whatSamp),,drop=FALSE])
-  if(length(whatSamp)==1 & whatSamp%in%obj$Meta$sType) SaData=lapply(obj$Data,function(x) x[which(obj$Meta$sType==whatSamp),,drop=FALSE])
+  lSampType=lSa=lQC=NULL
+  if(length(whatSamp)>0 & any(whatSamp%in%obj$Meta$Sid)) lSa=which(obj$Meta$Sid%in%whatSamp)
+  if(length(whatSamp)==1 & whatSamp[1]%in%obj$Meta$sType)  lSa=which(obj$Meta$sType==whatSamp)
+  if(length(lSa)>0){
+    lSampType=c(lSampType,"Sa")
+    cat(length(lSa),"sample found\n")
+    SaData=lapply(obj$Data,function(x) x[lSa,,drop=FALSE])
+  }
   
-  QCData=obj$Data
-  if(length(whatQC)>0 & whatQC%in%obj$Meta$Sid) QCData=lapply(obj$Data,function(x) x[which(obj$Meta$Sid%in%whatQC),,drop=FALSE])
-  if(length(whatQC)==1 & whatQC%in%obj$Meta$sType) QCData=lapply(obj$Data,function(x) x[which(obj$Meta$sType==whatQC),,drop=FALSE])
   
+  if(length(whatQC)>0 & any(whatQC%in%obj$Meta$Sid)) lQC=which(obj$Meta$Sid%in%whatQC)
+  if(length(whatQC)==1 & whatQC[1]%in%obj$Meta$sType)  lQC=which(obj$Meta$sType==whatQC)
+  if(length(lQC)>0){
+    lSampType=c(lSampType,"QC")
+    cat(length(lQC),"QC found\n")
+    QCData=lapply(obj$Data,function(x) x[lQC,,drop=FALSE])
+  }
+  if(length(lSampType)==0) stop("No samples found!\n")
   istats=lStats[[1]]
   allst=list()
   for(istats in lStats){
     if(!istats[1]%in%c("Sum","Prop","PropNNA","CV","CVr")){cat("Computation type must be Sum/Prop/PropNNA/CV/CVr for:",istats,"\n");next}
-    if(!istats[2]%in%c("QC","Sa")){cat("Computation type must be Sa/QC for:",istats,"\n");next}
+    if(!istats[2]%in%lSampType){cat("Computation type must be ",paste(lSampType,collapse="/")," for:",istats,"\n");next}
     
     if(istats[1]=="Sum") eval(parse(text=paste("re<-try(colSums(",istats[2],"Data$",istats[3],",na.rm=T),TRUE)",sep="")))
     if(istats[1]=="Prop")  eval(parse(text=paste("re<-try(colSums(",istats[2],"Data$",istats[3],",na.rm=T)*100/nrow(!is.na(",istats[2],"Data$",istats[3],")),TRUE)",sep="")))
@@ -33,9 +43,7 @@ compSetStats<-function(obj,lStats=list(),whatSamp="Sa",whatQC="QC",add=FALSE){
   if(add){
     if(!is.null(res)) obj$Annot=cbind(obj$Annot,res)
     invisible(obj)
-  }
-
-  if(!add) invisible(res)
+  } else invisible(res)
     
 }
 

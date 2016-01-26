2678cee57c24251de04ac33553df69be369f435f
tonedivad <notinaber@gmail.com>
2016-01-26 15:55:52 +0100

    modified:   DESCRIPTION 	new file:   R/internal.R

---
+++ R/internal.R
@@ -0,0 +1,5 @@
+.onAttach <- 
+  function(libname, pkgname) {
+    packageStartupMessage(" GRMeta by David Enot <david.enot@gustaveroussy.fr>\n.")
+    packageStartupMessage(" Source code available at https://github.com/tonedivad/GRMeta \n")
+  }

d046665da9e869760330c5872d71b4e232913394
tonedivad <notinaber@gmail.com>
2016-01-26 16:00:17 +0100

    modified:   R/internal.R

--- R/internal.R
+++ R/internal.R
@@ -1,5 +1,7 @@
 .onAttach <- 
   function(libname, pkgname) {
+    packageStartupMessage(" ****************************************************************************")
     packageStartupMessage(" GRMeta: processing chemical data by David Enot <david.enot@gustaveroussy.fr>")
     packageStartupMessage(" Source code available at https://github.com/tonedivad/GRMeta \n")
+    packageStartupMessage(" ****************************************************************************")
   }

e40355c6fdcfe7715d005d233fd18ebbf96bfbb7
tonedivad <notinaber@gmail.com>
2016-01-29 22:42:33 +0100

    modified:   R/print.metaboSet.R

--- R/print.metaboSet.R
+++ R/print.metaboSet.R
@@ -2,6 +2,15 @@ print.metaboSet<-function(x,length=12){
   
   cat("** ",length(x$Analyte)," analyte",ifelse(length(x$Analyte)==1,"","s"),": ",paste(names(table(x$Annot$Method)),"(",(table(x$Annot$Method)),") ",sep=""),"\n",sep="")
   cat("** Annotation level: ",paste(names(table(x$Annot$LevelAnnot)),"(",(table(x$Annot$LevelAnnot)),") ",sep=""),"\n",sep="")
+  
+  if("fluxoSet"%in%class(x)){
+    tab=as.matrix(table(x$Annot$Iso,x$Annot$LevelAnnot))
+    for(i in rownames(tab))
+      cat("  M",i,": ",paste(colnames(tab),"(",tab[i,],") ",sep="")," ",sep="")
+    cat("\n")
+  }
+  
+  
   n=min(length(x$Analyte),length)
   top=x$Analyte[1:n]
   add=length(x$Analyte)-n;if(add>0) top=c(top,paste("+",add,"entries"))

7b4eabf9d8f82b806a3b6a9d2b474bdb8490cc68
david <notinaber@gmail.com>
2017-01-03 15:50:37 +0100

    modified:   NAMESPACE 	modified:   R/utils.R

--- R/utils.R
+++ R/utils.R
@@ -35,3 +35,22 @@ printElapse<-function(strtime,bef="",add="\n"){
   }
   sprintf("%s%.2f %s%s",bef,top,units,add)
 }
+
+##############
+.GRgetAgilentXMLInfos<-function(cefi){
+  system.time(doc <- xmlRoot(xmlParse(cefi, useInternal = TRUE))[[2]])
+  inst=xmlChildren(xmlChildren(doc)$instrument)
+  dproc=xmlChildren(xmlChildren(doc)$dataProcessing)
+  top=do.call("cbind",lapply(c(xmlToList(inst$detector),xmlToList(inst$source),xmlToList(inst$additional),
+                               xmlToList(dproc$processingMethod)),function(x) x[c("name","value")]))
+  df=top["value",]
+  names(df)=top["name",]
+  df=c(unlist(xmlToList(xmlChildren(xmlChildren(doc)$admin)$sourceFile)),df)
+  df=data.frame(t(as.matrix(df,nrow=1)),stringsAsFactors = FALSE)
+  dts=xmlAttrs(dproc$software)["completionTime"]
+  dts = strsplit(gsub("\"", "", dts), "T")[[1]]
+  df$completionTime=chron(dts[1], dts[2], format = c(dates = "Y-M-D", times = "h:m:s"))
+  
+  invisible(df)
+}
+
